34701d1550649175c8de6751cc2deca1f89912ce
藤田優作 <fujitayuusaku@fujitayuusakunomacbook-pro.local>
2019-03-18 20:45:48 +0900

    Initial Commit

---
+++ Swift4Manganioto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift4Manganioto
+//
+//  Created by 藤田優作 on 2019/03/18.
+//  Copyright © 2019 藤田優作. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
